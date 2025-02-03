ce4e98e0f6c80651ca574e193aa2ceb7174cda47
eziz-tajiev <eziztajiev@gmail.com>
2025-02-04 01:05:24 +0500

    finished with buttons

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -22,14 +22,14 @@ export const HeroSection = () => {
           I specialize in transforming designs into functional, high-performing
           web applications. Let's discuss your next project.
         </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
-            <ArrowDown />
+        <div className='flex flex-col items-center mt-8 gap-4'>
+          <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
+            <span className='font-semibold'>Explore My Work</span>
+            <ArrowDown className='size-4' />
           </button>
-          <button>
+          <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl'>
             <span>👋🏽</span>
-            <span>Let's Connect</span>
+            <span className='font-semibold'>Let's Connect</span>
           </button>
         </div>
       </div>
