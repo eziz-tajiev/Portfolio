c867929cf193eb988741b01211414b25cdb2b633
eziz-tajiev <eziztajiev@gmail.com>
2025-02-04 00:20:21 +0500

    fininishe available section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,12 +3,19 @@ import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg'
 export const HeroSection = () => {
   return (
-    <div>
+    
+    <div className='py-32'>
       <div className='container'>
-        <Image src={memoji} alt='Person with laptop' />
-        <div>
-          <div></div>
-          <div>Available for new projects</div>
+        <div className='flex flex-col items-center'>
+          <Image 
+          src={memoji}
+          className='size-[100px] relative top-1'
+          alt='Person with laptop'
+          />
+          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
+            <div className='bg-green-500 size-2.5 rounded-full'></div>
+            <div className='text-sm font-medium'>Available for new projects</div>
+          </div>
         </div>
         <h1>Building Exceptional User Experiences</h1>
         <p>
@@ -25,8 +32,6 @@ export const HeroSection = () => {
             <span>Let's Connect</span>
           </button>
         </div>
-
-        
       </div>
     </div>
   );
