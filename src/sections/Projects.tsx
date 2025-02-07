3489ca9e2384e6bc5c4cf7550a241f211fa41176
eziz-tajiev <eziztajiev@gmail.com>
2025-02-07 17:37:46 +0500

    i ve started to style a ul of project section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,40 +2,41 @@ import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import Image from "next/image";
+import CheckIcon from "@/assets/icons/check-circle.svg"
 
 const portfolioProjects = [
   {
     company: "Project one",
     year: "2022",
-    title: "fitness website",
+    title: "Fitness Website",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Project Description" },
+      { title: "Project Description" },
+      { title: "Project Description" },
     ],
     link: "fitgym",
     image: darkSaasLandingPage,
   },
   {
-    company: "Project two",
+    company: "Project Two",
     year: "2021",
-    title: "github user search",
+    title: "Github User Search",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      { title: "Project Description" },
+      { title: "Project Description" },
+      { title: "Project Description" },
     ],
     link: "Github",
     image: lightSaasLandingPage,
   },
   {
-    company: "Project three",
+    company: "Project Three",
     year: "2023",
     title: "Nextjs Dashboard",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      { title: "Project Description" },
+      { title: "Project Description" },
+      { title: "Project Description" },
     ],
     link: "dashboard",
     image: aiStartupLandingPage,
@@ -54,18 +55,23 @@ export const ProjectsSection = () => {
         <div className="flex flex-col mt-10">
           {portfolioProjects.map((project) => (
             <div key={project.title} 
-                className="bg-gray-800 rounded-3xl overflow-hidden outline outline-2 outline-white/20 -outline-offset-2">
-              <div>
-                <div>
+                className="bg-gray-800 rounded-3xl overflow-hidden outline outline-2 outline-white/20 -outline-offset-2 p-8">
+              <div className="flex">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest
+                text-sm text-transparent bg-clip-text">
                   <span>{project.company}</span>
+                  <span>&bull;</span>
                   <span>{project.year}</span>
                 </div>
               </div>
-              <h3>{project.title}</h3>
-              <hr />
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4"/>
               <ul>
                 {project.results.map((result) => 
-                <li>{result.title}</li>
+                <li className="flex gap-2 text-sm text-white/50">
+                  <CheckIcon className="size-5" />
+                  <span>{result.title}</span>
+                </li>
               )}
               </ul>
               <a href="">
