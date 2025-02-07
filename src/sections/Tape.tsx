3709c08391fd5e2ed87a3b0116a1e242e658b123
eziz-tajiev <eziztajiev@gmail.com>
2025-02-08 00:37:54 +0500

    moved to type section and added array of words

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,16 @@
+const words = [
+  "Perfomant",
+  "Accsessibile",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Frindely",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+]
+
 export const TapeSection = () => {
   return <div>Tape Section</div>;
 };
