933e839c33e593761b9c64399df5313bc3848644
eziz-tajiev <eziztajiev@gmail.com>
2025-02-10 18:23:47 +0500

    created the sectionHeader

---
+++ src/components/SectionHeader.tsx
@@ -0,0 +1,26 @@
+
+export const SectionHeader = ({
+    title,
+    eyebrow,
+    description,
+}: {
+    title: string;
+    eyebrow: string;
+    description: string;
+}) => {
+    return (
+        <>
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+            {title}
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">
+            {eyebrow}
+        </h2>
+        <p className="text-center text-white/60 mt-4 md:text-lg lg:text-xl max-w-md mx-auto">
+            {description}
+        </p>
+        </>
+    )
+}
