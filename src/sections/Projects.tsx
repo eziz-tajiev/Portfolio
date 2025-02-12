9f5f57f7a49d1c5916b441b3decfe76358aaa652
eziz-tajiev <eziztajiev@gmail.com>
2025-02-12 20:13:21 +0500

    i have moved to About Section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -73,7 +73,7 @@ export const ProjectsSection = () => {
                   <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
                   <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                     {project.results.map((result) => 
-                    <li className="flex gap-2 text-white/50 text-sm md:text-base">
+                    <li key={project.title}className="flex gap-2 text-white/50 text-sm md:text-base">
                       <CheckIcon className="size-5 md:size-6" />
                       <span>{result.title}</span>
                     </li>
