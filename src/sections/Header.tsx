6d870bcb3dd401b22d04f9d3318489e53346b358
eziz-tajiev <eziztajiev@gmail.com>
2025-02-21 21:31:56 +0500

    changed the structure of header and start to animate

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,12 +1,64 @@
+// export const Header = () => {
+//   return (
+//    <div className="flex justify-center items-center fixed top-3 left-1/2 -translate-x-1/2 z-20">
+//     <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+//       <a href="#" className="nav-item">Home</a>
+//       <a href="#" className="nav-item">Projects</a>
+//       <a href="#" className="nav-item">About</a>
+//       <a href="#" className="nav-item bg-white text-gray-900">Contact</a>
+//     </nav>
+//   </div>
+//  );
+// };
+'use client'
+import { useState } from "react";
+import {motion} from "framer-motion";
+
+let tabs = [
+  {
+    id: "home",
+    label: "Home",
+    href: "#",
+  },
+  {
+    id: "projects",
+    label: "Projects",
+    href: "#",
+  },
+  {
+    id: "about",
+    label: "About",
+    href: "#",
+  },
+  {
+    id: "contact",
+    label: "Contact",
+    href: "#",
+  }
+]
+
 export const Header = () => {
+  let [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
-   <div className="flex justify-center items-center fixed top-3 left-1/2 -translate-x-1/2 z-20">
-    <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-      <a href="#" className="nav-item">Home</a>
-      <a href="#" className="nav-item">Projects</a>
-      <a href="#" className="nav-item">About</a>
-      <a href="#" className="nav-item bg-white text-gray-900">Contact</a>
-    </nav>
-  </div>
- );
+    <div>
+      <div className="flex justify-center items-center fixed top-3 left-1/2 -translate-x-1/2 z-20">
+        <div className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+          {tabs.map(tab=>(
+            <button
+                key={tab.id}
+                onClick={() => setActiveTab(tab.id)}
+                className={`${
+                  activeTab === tab.id ? "text-gray-900" : "hover:text-white/70"
+                } relative nav-item`}
+              >
+                {activeTab === tab.id && (
+                  <motion.div layoutId="active-pill" className="absolute bg-white inset-0 rounded-full"/>
+                )}
+                <span className="relative">{tab.label}</span>
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
