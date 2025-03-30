01910238ff0230b51455511df2f635e81453f764
eziz-tajiev <eziztajiev@gmail.com>
2025-03-30 18:25:47 +0500

    fixed bug with text selection in Hero component

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -12,6 +12,49 @@ export const HeroSection = () => {
   return (
     
     <div className='py-32 md:py-48 relative z-0 overflow-x-clip'>
+
+      <div className='relative z-10 container'>
+        <div className='flex flex-col items-center'>
+          <Image 
+          src={memoji}
+          className='size-[100px] relative top-1'
+          alt='Person with laptop'
+          />
+          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
+            <div className='relative bg-green-500 size-2.5 rounded-full'>
+              <motion.div 
+                className='absolute bg-green-500 inset-0 rounded-full'
+                initial={{scale:1, opacity:0.7}}
+                animate={{scale:2, opacity:0}}
+                transition={{
+                  duration: 2,
+                  ease: "easeInOut",
+                  repeat: Infinity,
+                }}
+              />
+            </div>
+            <div className='text-sm font-medium'>Available for new projects</div>
+          </div>
+        </div>
+        <div className='max-w-lg mx-auto'>
+          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>Building Exceptional User Experiences</h1>
+          <p className='mt-4 text-center text-white/60 md:text-lg'>
+            I specialize in transforming designs into functional, high-performing
+            web applications. Let's discuss your next project.
+          </p>
+        </div>
+        <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
+          <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
+            <span className='font-semibold'>Explore My Work</span>
+            <ArrowDown className='size-4' />
+          </button>
+          <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl'>
+            <span>👋🏽</span>
+            <span className='font-semibold'>Let's Connect</span>
+          </button>
+        </div>
+      </div>
+
       <div className='absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]'>
         <div className='absolute inset-0 -z-30 opacity-5' style={{
           backgroundImage: `url(${grainImage.src})`}}>
@@ -55,48 +98,6 @@ export const HeroSection = () => {
             <div className='size-2 rounded-full bg-emerald-300/20'></div>
           </HeroOrbit>
         </div>
-
-      <div className='container'>
-        <div className='flex flex-col items-center'>
-          <Image 
-          src={memoji}
-          className='size-[100px] relative top-1'
-          alt='Person with laptop'
-          />
-          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
-            <div className='relative bg-green-500 size-2.5 rounded-full'>
-              <motion.div 
-                className='absolute bg-green-500 inset-0 rounded-full'
-                initial={{scale:1, opacity:0.7}}
-                animate={{scale:2, opacity:0}}
-                transition={{
-                  duration: 2,
-                  ease: "easeInOut",
-                  repeat: Infinity,
-                }}
-              />
-            </div>
-            <div className='text-sm font-medium'>Available for new projects</div>
-          </div>
-        </div>
-        <div className='max-w-lg mx-auto'>
-          <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>Building Exceptional User Experiences</h1>
-          <p className='mt-4 text-center text-white/60 md:text-lg'>
-            I specialize in transforming designs into functional, high-performing
-            web applications. Let's discuss your next project.
-          </p>
-        </div>
-        <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
-          <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
-            <span className='font-semibold'>Explore My Work</span>
-            <ArrowDown className='size-4' />
-          </button>
-          <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl'>
-            <span>👋🏽</span>
-            <span className='font-semibold'>Let's Connect</span>
-          </button>
-        </div>
-      </div>
     </div>
   );
 };
