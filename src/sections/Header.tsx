91b9a551070d2761d3a9093b4ac53f705ca3c4b3
eziz-tajiev <eziztajiev@gmail.com>
2025-04-18 09:03:22 +0500

    fixed bug with header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 
@@ -23,45 +23,51 @@ let tabs = [
 ];
 
 export const Header = () => {
-  const [hash, setHash] = useState<string | undefined>(() => {
-    if (window != undefined) return `/${window.location.hash}`;
-  });
+  const [hash, setHash] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") setHash(`/${window.location.hash}`);
+  }, []);
 
   const router = useRouter();
 
   return (
     <header className="flex justify-center items-center fixed top-3 left-1/2 -translate-x-1/2 z-20">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        {tabs.map((tab) => (
-          <button
-            key={tab.href}
-            onClick={() => {
-              setHash(tab.href);
-              router.replace(tab.href);
-            }}
-            className={`${
-              hash === tab.href ? "" : "hover:text-white/70"
-            } relative nav-item`}
-          >
-            <motion.div
-              layoutId={"active-pill"}
-              className={`absolute inset-0 ${
-                tab.href == hash ? "bg-white" : ""
-              }`}
-              style={{
-                borderRadius: "9999px",
+        {tabs.map((tab) => {
+          console.log(hash === tab.href);
+
+          return (
+            <button
+              key={tab.href}
+              onClick={() => {
+                setHash(tab.href);
+                router.replace(tab.href);
               }}
-              transition={{
-                type: "spring",
-                duration: 0.6,
-              }}
-            />
-            {/* )} */}
-            <span className="relative z-10 mix-blend-exclusion">
-              {tab.label}
-            </span>
-          </button>
-        ))}
+              className={`${
+                hash === tab.href ? "" : "hover:text-white/70"
+              } relative nav-item`}
+            >
+              <motion.div
+                layoutId={"active-pill"}
+                className={`absolute inset-0 ${
+                  tab.href == hash ? "bg-white" : ""
+                }`}
+                style={{
+                  borderRadius: "9999px",
+                }}
+                transition={{
+                  type: "spring",
+                  duration: 0.6,
+                }}
+              />
+              {/* )} */}
+              <span className="relative z-10 mix-blend-exclusion">
+                {tab.label}
+              </span>
+            </button>
+          );
+        })}
       </nav>
     </header>
   );
